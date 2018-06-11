1d26aa364a7331c71a92b47ca0180e8b0978b460
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-11 10:14:45 -0700

    Add files via upload

---
+++ spacecount.py
@@ -0,0 +1,15 @@
+string=raw_input()
+
+list=['1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','^','&','*','(',')']
+
+str=0
+
+if string in list:
+	
+    print('Enter the valid input')
+
+else:
+            
+    str=string.count(' ')
+            
+    print str

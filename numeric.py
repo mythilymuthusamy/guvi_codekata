cf7c6580d88e9e242af2c174d195bab1ab2f86ca
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-11 09:32:06 -0700

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,11 @@
+num=raw_input()
+
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+if num in list:
+	
+    print('Given num is alhabet')
+
+else:
+	
+    print('Given num is number')

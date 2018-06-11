4a241767778a35fd30de120cc5d14110e44124aa
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-11 10:05:01 -0700

    Add files via upload

---
+++ countword.py
@@ -0,0 +1,20 @@
+string=raw_input()
+
+list=['1','2','3','4','5','6','7','8','9','0']
+
+str=0
+
+mystr=0
+
+if string in list:
+	
+    print('Enter the alphabet')
+
+else:
+	
+            
+    str=string.split(" ")
+            
+    mystr=len(str)
+            
+    print mystr

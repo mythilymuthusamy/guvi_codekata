2f47c6a48e054abf53dba954964242ff3ead341c
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-11 07:41:07 -0700

    Add files via upload

---
+++ time.py
@@ -0,0 +1,18 @@
+Time=int(input('Ente the time in miniute:\n'))
+
+h=0
+
+m=0
+
+if (Time>0):
+	
+    h=Time/60
+	
+    m=Time%60
+	
+    print(h,m)
+
+else:
+	
+    print('Enter the valid number')
+	

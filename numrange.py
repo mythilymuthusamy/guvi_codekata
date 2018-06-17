fbb391f69578e2a36cfacb11cdd5a52303b584a2
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-17 07:03:10 -0700

    Add files via upload

---
+++ numrange.py
@@ -0,0 +1,18 @@
+num=raw_input()
+
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+if num in list:
+	
+    print('Enter the valid number')
+
+else:
+	
+    if int(num) in range(1,10):
+		
+        print('yes')
+	
+    else:
+		
+        print('no')
+	

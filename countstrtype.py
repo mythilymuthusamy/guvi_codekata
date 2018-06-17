9ed0ec2249e03a22744ff192942c9c7f73e0c029
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-17 04:07:40 -0700

    Add files via upload

---
+++ countstrtype.py
@@ -0,0 +1,19 @@
+str1=raw_input()
+
+str2=raw_input()
+
+mystr1=len(str1)
+
+mystr2=len(str2)
+
+if(mystr1==mystr2):
+	
+    print str1
+
+elif(mystr1>mystr2):
+	
+    print str1
+
+else:
+	
+    print str2

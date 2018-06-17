8e32b49e4bc725b9a27b0812a066b243007b3a49
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-17 08:03:51 -0700

    Add files via upload

---
+++ average.py
@@ -0,0 +1,37 @@
+num1=raw_input()
+
+num2=raw_input()
+
+num3=raw_input()
+
+num4=raw_input()
+
+num5=raw_input()
+
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+if num1 in list:
+	
+    print('enter the number')
+
+if num2 in list:
+	
+    print('Enter the number')
+
+if num3 in list:
+	
+    print('Enter the number')
+
+if num4 in list:
+	
+    print('Enter the number')
+
+if num5 in list:
+	
+    print('Enter the number')
+
+else:
+            
+    average=(int(num1)+int(num2)+int(num3)+int(num4)+int(num5))/5
+   
+     print average

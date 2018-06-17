e1712c8892be637e2ac4a58cd7066299a9b9f720
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-17 06:12:41 -0700

    Add files via upload

---
+++ joinstr.py
@@ -0,0 +1,19 @@
+str1=raw_input()
+
+str2=raw_input()
+
+list=['1','2','3','4','5','6','7','8','9','0']
+
+if str1 in list:
+	
+    print('Enter the valid input')
+
+elif str2 in list:
+	
+    print('Enter the valid input')
+
+else:
+            
+    mystr=str1+str2
+           
+    print mystr

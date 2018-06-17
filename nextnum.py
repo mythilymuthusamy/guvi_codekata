1ccdc1235ca57465cfb55f2532a75260caee4fbd
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-17 07:14:25 -0700

    Add files via upload

---
+++ nextnum.py
@@ -0,0 +1,13 @@
+num=raw_input()
+
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+if num in list:
+	
+    print('Enter the number')
+
+else:
+	
+    mynum=int(num)+1
+	
+    print mynum

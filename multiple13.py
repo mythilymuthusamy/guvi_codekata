a0306442f09a9cb36d35c5140fd4e65bc206fdff
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-17 09:30:51 -0700

    Add files via upload

---
+++ multiple13.py
@@ -0,0 +1,18 @@
+num1=raw_input()
+
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+if num1 in list:
+	
+    print('enter the number')
+
+else:
+	
+	
+    if int(num1)%13==0:
+		
+        print('multiple of 13')
+	
+    else:
+		
+        print('not multiple of 13')

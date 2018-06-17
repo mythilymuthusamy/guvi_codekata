831be68d5ca02f95a985aa9e385d9a1bbe5968af
mythilymuthusamy <36659385+mythilymuthusamy@users.noreply.github.com>
2018-06-17 08:14:19 -0700

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,19 @@
+num1=raw_input()
+
+num2=raw_input()
+
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+if num1 in list:
+	
+    print('enter the number')
+
+if num2 in list:
+	
+    print('Enter the number')
+
+else:
+	
+    num3=int(num1)+int(num2)
+	
+    print num3
